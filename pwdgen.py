a83c59301b7229014c88a3ae6dc69b10000d2be6
Ludo <siket.ludovit@gmail.com>
2019-12-29 18:05:42 +0100

    Funkcia abeceda() vracia jeden slovnik.
    Odstranena funkcia counting_length().
    Odstranena funkcia make_pwd(), namiesto nej make_password().
    
    Funkcia collect_values().
    Nefunguje multi_replace().

--- pwdgen.py
+++ pwdgen.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-import random
-import sys
 import argparse
+import sys
+import random
 
 
 def abeceda():
-    m_pismena = ["a", "b", "c", "d", "e", "f", "g",
-                 "h", "i", "j", "k", "l", "m", "n", "o",
-                 "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                 "y", "z"]
-
-    symboly = [".", "!", "?", "_", "-", ";", "/", "=", "*",
-               "@", "#", "$", "%", "^", "&", " ", "+"]
-
-    cisla = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-    v_pismena = ["A", "B", "C", "D", "E", "F", "G",
-                 "H", "I", "J", "K", "L", "M", "N", "O",
-                 "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-                 "Y", "Z"]
-    return m_pismena, symboly, v_pismena, cisla
+    alphabet = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g",
+                8: "h", 9: "i", 10: "j", 11: "k", 12: "l", 13: "m", 14: "n", 15: "o",
+                16: "p", 17: "q", 18: "r", 19: "s", 20: "t", 21: "u", 22: "v", 23: "w", 24: "x",
+                25: "y", 26: "z",
+                27: ".", 28: "!", 29: "?", 30: "_", 31: "-", 32: ";", 33: "/", 34: "=", 35: "*",
+                36: "@", 37: "#", 38: "$", 39: "%", 40: "^", 41: "&", 42: " ", 43: "+",
+                44: "0", 45: "1", 46: "2", 47: "3", 48: "4", 49: "5", 50: "6", 51: "7", 52: "8", 53: "9",
+                54: "A", 55: "B", 56: "C", 57: "D", 58: "E", 59: "F", 60: "G",
+                61: "H", 62: "I", 63: "J", 64: "K", 65: "L", 66: "M", 67: "N", 68: "O",
+                69: "P", 70: "Q", 71: "R", 72: "S", 73: "T", 74: "U", 75: "V", 76: "W", 77: "X",
+                78: "Y", 79: "Z"}
+    return alphabet
 
 
 def multi_replace(content, to_replace, repl):
@@ -29,44 +26,33 @@ def multi_replace(content, to_replace, repl):
     return content
 
 
-def counting_lenght(abc):
-    index = 1
-    indexed_list = []
-    while index <= len(abc):
-        value = str(len("list" + str(index)))
-        indexed_list.append(value)
-        index += 1
-    whole_min = int(min(indexed_list[0], indexed_list[1], indexed_list[2], indexed_list[3]))
-    return indexed_list[0], indexed_list[1], indexed_list[2], indexed_list[3], whole_min
-
-
-def make_pwd(l1, l2, l3, l4, some_list):
-    rnd1 = random.randrange(int(l1))
-    rnd2 = random.randrange(int(l2))
-    rnd3 = random.randrange(int(l3))
-    rnd4 = random.randrange(int(l4))
-    some_list += (male_pismena[rnd1], znaky[rnd2], velke_pismena[rnd3], cislice[rnd4])
-    return some_list
-
-
 def pwdgen_help():
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('-l', '--length', action="store", dest="l", type=int, help="number of password length, max 79")
     parser.print_help()
 
 
+def collect_values():
+    full_dict = abeceda()
+    whole_length = len(full_dict)
+    return full_dict, whole_length
+
+
+def make_password(value_length, values, length):
+    iterator = 0
+    password = []
+    while iterator < value_length:
+        dict_key = (random.randrange(length))
+        password.append(values.get(dict_key))
+        iterator += 1
+    return password
+
+
 if __name__ == '__main__':
     try:
-        dlzka_hesla = abs(int(sys.argv[1]))
-        male_pismena, znaky, velke_pismena, cislice = abeceda()
-        m, z, v, c, pwd_len = counting_lenght(abeceda())
-        whole_list = []
-        counter = 0
-        while counter < pwd_len:
-            whole_list = make_pwd(m, z, v, c, whole_list)
-            counter += 1
-        heslo = str(whole_list[0:dlzka_hesla])
-        heslo = multi_replace(heslo, [',', "'", ' ', '[', ']'], '')
-        print(heslo)
+        password_length = abs(int(sys.argv[1]))
+        dictionary, dictionary_length = collect_values()
+        pwd = make_password(password_length, dictionary, dictionary_length)
+        print(pwd)
     except (IndexError, ValueError):
         pwdgen_help()

8b636208a9afdb94d53940630b4bb8331daf2fe9
Ludo <siket.ludovit@gmail.com>
2020-01-01 15:41:54 +0100

    Funkcna multi_replace().
    Funkcia make_dict().
    Funkcie dict_keys() a dict_values().
    Odstranena funkcia abeceda().

--- pwdgen.py
+++ pwdgen.py
@@ -4,19 +4,27 @@ import sys
 import random
 
 
-def abeceda():
-    alphabet = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g",
-                8: "h", 9: "i", 10: "j", 11: "k", 12: "l", 13: "m", 14: "n", 15: "o",
-                16: "p", 17: "q", 18: "r", 19: "s", 20: "t", 21: "u", 22: "v", 23: "w", 24: "x",
-                25: "y", 26: "z",
-                27: ".", 28: "!", 29: "?", 30: "_", 31: "-", 32: ";", 33: "/", 34: "=", 35: "*",
-                36: "@", 37: "#", 38: "$", 39: "%", 40: "^", 41: "&", 42: " ", 43: "+",
-                44: "0", 45: "1", 46: "2", 47: "3", 48: "4", 49: "5", 50: "6", 51: "7", 52: "8", 53: "9",
-                54: "A", 55: "B", 56: "C", 57: "D", 58: "E", 59: "F", 60: "G",
-                61: "H", 62: "I", 63: "J", 64: "K", 65: "L", 66: "M", 67: "N", 68: "O",
-                69: "P", 70: "Q", 71: "R", 72: "S", 73: "T", 74: "U", 75: "V", 76: "W", 77: "X",
-                78: "Y", 79: "Z"}
-    return alphabet
+def dict_keys(values):
+    key_length = len(values)
+    x = 0
+    k = []
+    while x < key_length:
+        k.append(x)
+        x += 1
+    return k
+
+
+def dict_values():
+    values = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+              "v", "w", "x", "y", "z", ".", "!", "?", "_", "-", ";", "/", "=", "*", "@", "#", "$", "%", "^", "&", " ",
+              "+", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G",
+              "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    return values
+
+
+def make_dict(list1, list2):
+    result = dict(zip(list1, list2))
+    return result
 
 
 def multi_replace(content, to_replace, repl):
@@ -33,7 +41,7 @@ def pwdgen_help():
 
 
 def collect_values():
-    full_dict = abeceda()
+    full_dict = result_dict
     whole_length = len(full_dict)
     return full_dict, whole_length
 
@@ -45,14 +53,18 @@ def make_password(value_length, values, length):
         dict_key = (random.randrange(length))
         password.append(values.get(dict_key))
         iterator += 1
-    return password
+    return str(password)
 
 
 if __name__ == '__main__':
     try:
         password_length = abs(int(sys.argv[1]))
+        val = dict_values()
+        keys = dict_keys(dict_values())
+        result_dict = make_dict(keys, val)
         dictionary, dictionary_length = collect_values()
         pwd = make_password(password_length, dictionary, dictionary_length)
-        print(pwd)
+        nice_pwd = multi_replace(pwd, '[,],' ',' "'", '')
+        print(nice_pwd)
     except (IndexError, ValueError):
         pwdgen_help()

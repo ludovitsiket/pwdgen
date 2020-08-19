c18ead48157c57c7b7724aa5ab59407a46ed43fc
ludovitsiket <siket.ludovit@gmail.com>
2020-08-19 15:19:39 +0200

    Nice code.

--- pwdgen.py
+++ pwdgen.py
@@ -15,10 +15,14 @@ def dict_keys(values):
 
 
 def dict_values():
-    values = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-              "v", "w", "x", "y", "z", ".", "!", "?", "_", "-", ";", "/", "=", "*", "@", "#", "$", "%", "^", "&", " ",
-              "+", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G",
-              "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    values = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+              "n", "o", "p", "q", "r", "s", "t", "u",
+              "v", "w", "x", "y", "z", ".", "!", "?", "_", "-", ";", "/", "=",
+              "*", "@", "#", "$", "%", "^", "&", " ",
+              "+", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B",
+              "C", "D", "E", "F", "G",
+              "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+              "U", "V", "W", "X", "Y", "Z"]
     return values
 
 
@@ -36,8 +40,10 @@ def multi_replace(content, to_replace, repl):
 
 def pwdgen_help():
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument('-l', '--length', action="store", dest="l", type=int, help="number of password length, max 79")
-    parser.add_argument('-n', action="store", dest="n", help="password without special characters")
+    parser.add_argument('-l', '--length', action="store", dest="l", type=int,
+                        help="number of password length, max 79 numbers")
+    parser.add_argument('-n', action="store", dest="n",
+                        help="password without special characters")
     parser.print_help()
 
 
@@ -67,7 +73,8 @@ def no_characters():
 
 
 def replace_string(value):
-    symbols = (".", "!", "?", "_", "-", ";", "/", "=", "*", "@", "#", "$", "%", "^", "&", " ", "+")
+    symbols = (".", "!", "?", "_", "-", ";", "/", "=", "*", "@", "#", "$", "%",
+               "^", "&", " ", "+")
     num = str(random.randrange(0, 9))
     new_string = multi_replace(value, symbols, num)
     return new_string
